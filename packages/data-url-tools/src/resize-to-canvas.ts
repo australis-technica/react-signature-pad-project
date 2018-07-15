79dca854d3fdcb0d2aa3c8edb67f0ee1e35ce773
d10221 <danieliozzi@gmail.com>
2018-07-15 09:55:12 +0200

    data-url-tools: resize-to-canvas: parameters

--- packages/data-url-tools/src/resize-to-canvas.ts
+++ packages/data-url-tools/src/resize-to-canvas.ts
@@ -1,66 +1,65 @@
-/** */
+/**
+ * TODO: attribute original source/alg credits,
+ * just made it async/parametrized
+ */
 export default function resizeToCanvas(dataUrl: string, ratio: number) {
-    /** */
-    return new Promise<HTMLCanvasElement>((resolve, reject) => {
-      try {
-        const img = document.createElement("img");
-        img.src = dataUrl;
-        img.onload = () => {
-  
-          var canvas = document.createElement("canvas"),
-            ctx = canvas.getContext("2d"),
-            oc = document.createElement("canvas"),
-            octx = oc.getContext("2d");
-  
-          const width = img.width * ratio; // destination canvas size
-          canvas.width = width;
-          canvas.height = (canvas.width * img.height) / img.width;
-  
-          var cur = {
-            width: Math.floor(img.width * 0.5),
-            height: Math.floor(img.height * 0.5)
+  /** */
+  return new Promise<HTMLCanvasElement>((resolve, reject) => {
+    try {
+      const img = document.createElement("img");
+      img.src = dataUrl;
+      img.onload = () => {
+        const canvas = document.createElement("canvas");
+        // final width
+        const width = img.width * ratio;
+        canvas.width = width;
+        canvas.height = (canvas.width * img.height) / img.width;
+        // ...
+        var cur = {
+          width: Math.floor(img.width * ratio),
+          height: Math.floor(img.height * ratio)
+        };
+        const oc = document.createElement("canvas");
+        oc.width = cur.width;
+        oc.height = cur.height;
+        // ...
+        const octx = oc.getContext("2d");
+        octx.drawImage(img, 0, 0, cur.width, cur.height);
+        while (cur.width * ratio > width) {
+          cur = {
+            width: Math.floor(cur.width * ratio),
+            height: Math.floor(cur.height * ratio)
           };
-  
-          oc.width = cur.width;
-          oc.height = cur.height;
-  
-          octx.drawImage(img, 0, 0, cur.width, cur.height);
-  
-          while (cur.width * 0.5 > width) {
-            cur = {
-              width: Math.floor(cur.width * 0.5),
-              height: Math.floor(cur.height * 0.5)
-            };
-            octx.drawImage(
-              oc,
-              0,
-              0,
-              cur.width * 2,
-              cur.height * 2,
-              0,
-              0,
-              cur.width,
-              cur.height
-            );
-          }
-  
-          ctx.drawImage(
+          octx.drawImage(
             oc,
             0,
             0,
+            cur.width * ratio,
+            cur.height * ratio,
+            0,
+            0,
             cur.width,
-            cur.height,
-            0,
-            0,
-            canvas.width,
-            canvas.height
+            cur.height
           );
-  
-          resolve(canvas);
-        };
-      } catch (error) {
-        reject(error);
-      }
-    });
-  }
-  
+        }
+        // ...
+        const ctx = canvas.getContext("2d");
+        ctx.drawImage(
+          oc,
+          0,
+          0,
+          cur.width,
+          cur.height,
+          0,
+          0,
+          canvas.width,
+          canvas.height
+        );
+
+        resolve(canvas);
+      };
+    } catch (error) {
+      reject(error);
+    }
+  });
+}

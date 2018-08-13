df0f5ca512047d1eb2d466e13f868d56f1cfe5e3
d10221 <danieliozzi@gmail.com>
2018-08-13 07:13:41 +0200

    repo/set

--- packages/signature-pad-demo-backend/src/repos/images/set.ts
+++ packages/signature-pad-demo-backend/src/repos/images/set.ts
@@ -2,23 +2,13 @@ import { execSql as ExecSql } from "@australis/tiny-sql";
 import { Connection } from "tedious";
 import { ImageDto } from "./types";
 import { ensureBuffer } from "./ensure-buffer";
-import { debugModule } from "@australis/create-debug";
 import { TABLENAME } from "./constants";
-// ...
-const debug = debugModule(module);
-/** */
-export default async function set(connection: Connection, imageDto: ImageDto) {
+/** TODO: */
+export default async function set(connection: Connection, dto: ImageDto) {
     const execSql = ExecSql(connection);
-    return execSql<ImageDto>(`
-    UPDATE * from ${TABLENAME} where id = @id
+    await  execSql<ImageDto>(`
+    UPDATE ${TABLENAME} set img = @img where id = @id
     ` // ,,,
-        , ensureBuffer(imageDto)) // ...
-        .then(x => {
-            const { error, ...rest } = x;
-            if (error) {
-                debug(error);
-                return Promise.reject(x.error);
-            }
-            return Promise.resolve(rest.values[0]);
-        });
+        , ensureBuffer(dto));
+    return await  execSql<ImageDto>(`select top 1 * from ${TABLENAME} where id=@id`, dto);
 }

a8c254d946eecbf5930215af3069b6b821d37ce9
me <danieliozzi@gmail.com>
2018-08-13 09:07:56 +0200

    demo: backend: let parameter error so we can see it

--- packages/signature-pad-demo-backend/src/repos/images/add.ts
+++ packages/signature-pad-demo-backend/src/repos/images/add.ts
@@ -1,33 +1,37 @@
 import { execSql as ExecSql } from "@australis/tiny-sql";
 import { Connection } from "tedious";
 import { ImageDto } from "./types";
-import { ensureBuffer } from "./ensure-buffer";
+import { dataUrlToBuffer } from "../../img"
 import { debugModule } from "@australis/create-debug";
 import { TABLENAME } from "./constants";
 // ...
 const debug = debugModule(module);
 /** */
-export default async function add(connection: Connection, imageDto: ImageDto): Promise<ImageDto> {
+export default async function add(connection: Connection, dto: ImageDto): Promise<ImageDto> {
     const execSql = ExecSql(connection);
-    if (imageDto.id) {
-
-    }
-    if (imageDto.id.trim() === "") return Promise.reject(new Error("Empty ID"));
+    const { id } = dto;
+    if (!id || !id.trim()) return Promise.reject(new Error("Empty ID"));
+    if (!dto.img) return Promise.reject(new Error("img is required"));
     // ...
     const query = `
     insert into ${TABLENAME} (id, img) VALUES (@id, @img)
     ` ;
-    const params = ensureBuffer(imageDto);
-    if (params.img && params.img instanceof Buffer) {
-        if (params.img.length > 8000) return Promise.reject(new Error("Image is too big"));
-        // Zero buffer
-        if (!params.img.length) return Promise.reject(new Error("Can't add empty image"));
-    }
+    const img = typeof dto.img === "string" ? dataUrlToBuffer(dto.img) : dto.img;
+    
+    /** 
+     *  Let it error so we can see it.
+     */
+    
+     // if (params.img && params.img instanceof Buffer) {
+    //     if (params.img.length > 8000) return Promise.reject(new Error("Image is too big"));
+    //     // Zero buffer
+    //     if (!params.img.length) return Promise.reject(new Error("Can't add empty image"));
+    // }
 
-    const { error } = await execSql(query, params) // ...
+    const { error } = await execSql(query, { id, img }) // ...
     if (error) {
         debug(error);
         return Promise.reject(error);
     }
-    return Promise.resolve(imageDto);
+    return Promise.resolve(dto);
 }
